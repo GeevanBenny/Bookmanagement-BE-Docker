f700c8147c72601d65b6bd4c87a6183771fa9ed8
GeevanBenny <geevanbenny2605@gmail.com>
2024-12-13 15:40:41 +0530

    cors

--- src/main/java/com/book/config/WebConfig.java
+++ src/main/java/com/book/config/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("https://bookmanagement-fe-docker.vercel.app") // React frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/api/**")  // Applying CORS settings to all API routes
+                .allowedOrigins("https://bookmanagement-fe-docker.vercel.app")  // React frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow cookies and authentication
     }
 }
