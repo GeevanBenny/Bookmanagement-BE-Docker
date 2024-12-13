49e6801a996c6a4609e98e5f6225c3836397c416
GeevanBenny <geevanbenny2605@gmail.com>
2024-12-13 17:20:52 +0530

    Be link Added

--- src/main/java/com/book/config/WebConfig.java
+++ src/main/java/com/book/config/WebConfig.java
@@ -10,14 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // Apply CORS settings to all API routes
-                .allowedOrigins("https://bookmanagement-fe-docker.vercel.app")  // React frontend URL
+                .allowedOrigins("https://bookmanagement-fe-docker-2.onrender.com")  // React frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow cookies and authentication
 
         // If your frontend is trying to access non-API routes like `/form`, you can add this
         registry.addMapping("/**") // Apply CORS to all other routes
-                .allowedOrigins("https://bookmanagement-fe-docker.vercel.app")
+                .allowedOrigins("https://bookmanagement-fe-docker-2.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
